08de917f9f2450c68a9fcd8de377d17a2159b422
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2014-10-16 14:21:59 +0200

    all in PPSTrackerModule

--- Geometry/CMSCommonData/python/vuoto_e_PPS_cfi.py
+++ Geometry/CMSCommonData/python/vuoto_e_PPS_cfi.py
@@ -6,24 +6,23 @@ XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
                                                                    'Geometry/CMSCommonData/data/normal/cmsextent.xml', 
                                                                    'Geometry/CMSCommonData/data/cms.xml', 
 
-      'Geometry/CMSCommonData/data/cmsMother.xml', 
-	'Geometry/TotemRPData/data/TotemRPGlobal.xml',
+#      'Geometry/CMSCommonData/data/cmsMother.xml', 
+#	'Geometry/TotemRPData/data/TotemRPGlobal.xml',
                                                                  
-        'Geometry/TotemRPData/data/PPS_Prova_Assembly.xml',
-#'Geometry/TrackerCommonData/data/trackermaterial.xml',
+#        'Geometry/TotemRPData/data/PPS_Prova_Assembly.xml',
+
+
+      'Geometry/PPSTrackerData/data/cmsMother.xml', 
+	'Geometry/PPSTrackerData/data/TotemRPGlobal.xml',
+                                                                 
+        'Geometry/PPSTrackerData/data/PPS_Prova_Assembly.xml',
+
 'Geometry/PPSTrackerData/data/ppstrackerMaterials.xml',
 
                                                                    'Geometry/PPSTrackerData/data/PPSTrackerModule.xml',
 
 
-#                                                                   'Geometry/CMSCommonData/data/cmsMotherPilot.xml'
 
-
-
-
-
-
- #       'Geometry/CMSCommonData/data/FieldParameters.xml'
 ),
     rootNodeName = cms.string('cms:OCMS')
 )
